050c6a698d364d527bf0ba1c29eb23adb399b5de
Akshaya Rajagopalan <akshaya.rajagopalan@vidyard.com>
2019-01-11 09:26:57 -0500

    Validation checks added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
     has_many :comments
+    validates :content, length: {minimum: 50},
+                        presence: true
+    validates :title, length: {maximum: 50},
+                        presence: true
 end
